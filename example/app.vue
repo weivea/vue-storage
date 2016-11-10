7fd0c8e745da97793df9a567c33bf55a7629bba6
魏建立 <550281353@qq.com>
2016-11-10 14:23:15 +0800

    修复

--- example/app.vue
+++ example/app.vue
@@ -43,7 +43,6 @@
             }
         },
         created:function(){
-            console.log(this);
             setTimeout(()=>{
                 this.$twig.session.form.a = 123
             },5000);
